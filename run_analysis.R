9e2908d270d7a1f263d690951825775dde36158f
ranieri84 <vinicius.ranieri@gmail.com>
2020-06-18 19:24:54 -0300

    fixed small bug

--- run_analysis.R
+++ run_analysis.R
@@ -27,6 +27,8 @@ subjMerge<-rbind(SubjTrain,SubjTest)
 colvarnames <- data.table(read.table(file.path(
     path, 'UCI HAR Dataset','features.txt')))[,2]
 names(dfMerge_X)<-unlist(colvarnames)
+names(dfMerge_X)<-gsub("^t", "time", names(dfMerge_X))
+names(dfMerge_X)<-gsub("^f", "frequency", names(dfMerge_X))
 
 # Select only columns with mean and Std 
 dfFinal=dfMerge_X
@@ -41,6 +43,17 @@ dfFinal$Activity<-factor(dfFinal$Activity,levels = c(1,2,3,4,5,6),
                          labels = c("WALKING","WALKING_UPSTAIRS",
                                     "WALKING_DOWNSTAIRS","SITTING",
                                     "STANDING","LAYING"))
+
 # Creat Convert subjects to a factor structure
 dfFinal$Subject<-factor(dfFinal$Subject)
-#
+
+# Create new DF with mean grouped by Activity and Subject
+suppressWarnings(cleandata <- aggregate(dfFinal, by = list(dfFinal$Subject, dfFinal$Activity), FUN = mean,drop = TRUE))
+colnames(cleandata)[1] <- "Subject"
+names(cleandata)[2] <- "Activity"
+
+#remove last 2 unused columns
+cleandata<-cleandata[1:81]
+
+#write tidydata
+write.table(cleandata,file = "tidydata.txt", row.names = FALSE)
