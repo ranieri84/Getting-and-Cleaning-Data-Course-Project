c78e0c9bca4acd860a20c7822ecbd7fbdec71954
ranieri84 <vinicius.ranieri@gmail.com>
2020-06-18 19:10:00 -0300

    added run_analysis script

---
+++ run_analysis.R
@@ -0,0 +1,46 @@
+library(data.table)
+library(dplyr)
+path <- getwd()
+
+#read subjects
+SubjTrain <- data.table(read.table(file.path(
+    path,'UCI HAR Dataset', 'train', 'subject_train.txt')))
+SubjTest <- data.table(read.table(file.path(
+    path, 'UCI HAR Dataset', 'test', 'subject_test.txt')))
+
+# read data
+dfTrain_X <- data.table(read.table(file.path(
+    path,'UCI HAR Dataset', 'train', 'X_train.txt')))
+dfTrain_Y <- data.table(read.table(file.path(
+    path,'UCI HAR Dataset', 'train', 'y_train.txt')))
+dfTest_X <- data.table(read.table(file.path(
+    path, 'UCI HAR Dataset', 'test', 'X_test.txt')))
+dfTest_Y <- data.table(read.table(file.path(
+    path, 'UCI HAR Dataset', 'test', 'y_test.txt')))
+
+# merge files
+dfMerge_X<-rbind(dfTrain_X,dfTest_X)
+dfMerge_Y<-rbind(dfTrain_Y,dfTest_Y)
+subjMerge<-rbind(SubjTrain,SubjTest)
+
+# Attribute meaningful  variable names
+colvarnames <- data.table(read.table(file.path(
+    path, 'UCI HAR Dataset','features.txt')))[,2]
+names(dfMerge_X)<-unlist(colvarnames)
+
+# Select only columns with mean and Std 
+dfFinal=dfMerge_X
+newcolun<-grep("mean|std",colvarnames$V2)
+dfFinal<-dfFinal[,..newcolun] 
+
+# Add Subject and Activity columns
+dfFinal[,(c("Subject","Activity"))]<-c(subjMerge,dfMerge_Y)
+
+# Attribute descriptive activity names
+dfFinal$Activity<-factor(dfFinal$Activity,levels = c(1,2,3,4,5,6),
+                         labels = c("WALKING","WALKING_UPSTAIRS",
+                                    "WALKING_DOWNSTAIRS","SITTING",
+                                    "STANDING","LAYING"))
+# Creat Convert subjects to a factor structure
+dfFinal$Subject<-factor(dfFinal$Subject)
+#
